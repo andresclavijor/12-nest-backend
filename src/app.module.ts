bd3acf3e1727971e1394addfb02837414e51c3c1
Andres Clavijo~ <shipoca2012@hotmail.com>
2023-07-26 18:56:40 -0500

    Se añade mongo y se genera crud auth module

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [AuthModule, MongooseModule.forRoot('mongodb://localhost:27017')],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
