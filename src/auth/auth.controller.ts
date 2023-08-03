2210268368c309256959f4f31565141dbe9faecf
Andres Clavijo~ <shipoca2012@hotmail.com>
2023-08-03 18:54:35 -0500

    Se añade autenticacion

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,29 +1,58 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { CreateUserDto, LoginDto, RegisterUserDto, UpdateUserDto } from './dto';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginResponse } from './interfaces/login-response';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  create(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto);
+    return this.authService.create(createUserDto);
   }
 
+  @Post('/login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+
+  @Post('/register')
+  register(@Body() registerDto: RegisterUserDto) {
+    return this.authService.register(registerDto);
+  }
+
+  @UseGuards(AuthGuard)
   @Get()
-  findAll() {
+  findAll(@Request() req: Request) {
     return this.authService.findAll();
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('/check-token')
+  checkToken(@Request() req: Request) {
+    return this.authService.checkToken(req);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
+    return this.authService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+  update(@Param('id') id: string, @Body() updateAuthDto: UpdateUserDto) {
     return this.authService.update(+id, updateAuthDto);
   }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,108 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './entities/user.entity';
+import * as bcryptJs from 'bcryptjs';
+
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-Payload';
+import { LoginResponse } from './interfaces/login-response';
+import { CreateUserDto, LoginDto, RegisterUserDto, UpdateUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const { password, ...userData } = createUserDto;
+
+      const newUser = new this.userModel({
+        password: bcryptJs.hashSync(password, 10),
+        ...userData,
+      });
+      await newUser.save();
+      const { password: _, ...user } = newUser.toJSON();
+      return user;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(`${createUserDto.email} already exists!`);
+      }
+      throw new InternalServerErrorException('Somenthing worng!!!');
+    }
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async login(loginDto: LoginDto): Promise<LoginResponse> {
+    const { email, password } = loginDto;
+
+    const user = await this.userModel.findOne({ email }).exec();
+    if (!user) {
+      throw new UnauthorizedException('Not valid Credentials - email');
+    }
+
+    if (!bcryptJs.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Not valid Credentials - password');
+    }
+
+    const { password: _, ...rest } = user.toJSON();
+
+    return {
+      user: rest,
+      token: await this.getJwt({ id: user.id }),
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+  async register(registerDto: RegisterUserDto): Promise<LoginResponse> {
+    const user = await this.create(registerDto);
+
+    return {
+      user: user,
+      token: await this.getJwt({ id: user._id }),
+    };
   }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
+  async checkToken(req: Request): Promise<LoginResponse> {
+    const user = req['user'];
+    return {
+      user,
+      token: await this.getJwt({ id: user._id }),
+    };
+  }
+
+  findAll(): Promise<User[]> {
+    return this.userModel.find();
+  }
+
+  async findUserById(id: string) {
+    const user = await this.userModel.findById(id).exec();
+    const { password, ...rest } = user.toJSON();
+    return rest;
+  }
+
+  findOne(id: string) {
+    return this.userModel.findById(id);
+  }
+
+  update(id: number, updateAuthDto: UpdateUserDto) {
     return `This action updates a #${id} auth`;
   }
 
   remove(id: number) {
     return `This action removes a #${id} auth`;
+  }
+
+  async getJwt(payload: JwtPayload) {
+    const token = await this.jwtService.signAsync(payload);
+    return token;
   }
 }
